a4dd0cfdeabd7b60ee8d11a6f24726b188b3c6e8
emanueltewolde <etewolde@andrew.cmu.edu>
2026-01-07 18:35:34 -0500

    mechanism registry

--- src/registry/mechanism_registry.py
+++ src/registry/mechanism_registry.py
@@ -1,15 +1,19 @@
-from src.mechanisms.base import NoMechanism
+from src.mechanisms.no_mechanism import NoMechanism
 from src.mechanisms.disarmament import Disarmament
 from src.mechanisms.mediation import Mediation
 from src.mechanisms.repetition import Repetition
-from src.mechanisms.reputation import (ReputationPrisonersDilemma,
-                                       ReputationPublicGoods)
+from src.mechanisms.contracting import Contracting
+# Temporarily commented out - collaborator working on reputation mechanisms
+# from src.mechanisms.reputation import (ReputationPrisonersDilemma,
+#                                        ReputationPublicGoods)
 
 MECHANISM_REGISTRY = {
-    "ReputationPrisonersDilemma": ReputationPrisonersDilemma,
+    # Temporarily disabled - collaborator working on reputation mechanisms
+    # "ReputationPrisonersDilemma": ReputationPrisonersDilemma,
+    # "ReputationPublicGoods": ReputationPublicGoods,
     "NoMechanism": NoMechanism,
-    "ReputationPublicGoods": ReputationPublicGoods,
     "Repetition": Repetition,
     "Disarmament": Disarmament,
     "Mediation": Mediation,
+    "Contracting": Contracting,
 }
